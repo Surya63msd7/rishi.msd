ba27abe09813b855073af2dd82ae3d5267512bb8
Suryansh <ysuryansh576@gmail.com>
2026-06-19 01:32:16 +0530

    RIP

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-let a = 12;
+let nm = document.querySelector("#name");
+let form = document.querySelector("form");
 
-function abcd() {
-  console.log("HeHe");
-}
 
-function hui() {
-  a++;
-  console.log(a);
-}
+form.addEventListener("submit", function (dets) {
+  dets.preventDefault();
 
-abcd();
-hui();
+  if (nm.value.length <= 2) {
+    document.querySelector("#hide").style.display = "initial";
+  }
+  else {
+    document.querySelector("#hide").style.display = "none";
+  };
+})
