82632360e2045198ef684b489ab609ec79c3cb7d
Suryansh <ysuryansh576@gmail.com>
2026-06-27 04:29:37 +0530

    Toaster

---
+++ Toaster/indi.js
@@ -0,0 +1,26 @@
+function createToaster(config) {
+  return function (notification) 
+    {
+    let div = document.createElement("div");
+    div.className = `fixed bg-gray-800 text-white px-6 py-3 rounded shadow-lg
+    pointer-events-none ${config.positionX === "right" ? "right-10" : "left-10"}
+    ${config.positionY === "top" ? "top-10" : "bottom-10"}`;
+
+    div.textContent = notification;
+
+    document.body.appendChild(div);
+
+    setTimeout(() => {
+      document.body.removeChild(div);
+    }, config.duration * 1000);
+  };
+}
+
+    let toaster = createToaster({
+      positionX: "right",
+      positionY: "top",
+      theme: "dark",
+      duration: 3,
+    })
+  
+    toaster("this is a dummy message")
